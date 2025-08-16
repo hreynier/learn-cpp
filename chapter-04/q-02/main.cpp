d554769376b12daea55c82b4fffd81b109339a35
Harvey Reynier <56921263+hreynier@users.noreply.github.com>
2025-08-16 15:52:48 +0100

    Chapter 4 - Q2 solution

---
+++ chapter-04/q-02/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+double getUserDouble() {
+  double inp{};
+  std::cout << "Enter a double: ";
+  std::cin >> inp;
+
+  return inp;
+}
+
+char getUserSymbol() {
+  char inp{};
+  std::cout << "Enter a symbol: '+', '-', '*', or '/': ";
+  std::cin >> inp;
+
+  return inp;
+  if (inp == '+' || inp == '-' || inp == '*' || inp == '/') {
+    std::cout << "You entered: " << inp << std::endl;
+  } else {
+    std::cout << "You entered an invalid symbol: " << inp << std::endl;
+  }
+
+  return inp;
+}
+
+bool isValidSymbol(char inp) {
+  if (inp == '+' || inp == '-' || inp == '*' || inp == '/') {
+    std::cout << "You entered: " << inp << std::endl;
+    return true;
+  } else {
+    std::cout << "You entered an invalid symbol: " << inp << std::endl;
+    return false;
+  }
+}
+
+double getAnswer(double first, double second, char symbol) {
+  if (symbol == '+') {
+    return first + second;
+  } else if (symbol == '-') {
+    return first - second;
+  } else if (symbol == '/') {
+    return first / second;
+  } else if (symbol == '*') {
+    return first * second;
+  } else {
+    return 0.0;
+  }
+}
+
+int main() {
+  double first{getUserDouble()};
+  double second{getUserDouble()};
+  char symbol{getUserSymbol()};
+  if (isValidSymbol(symbol)) {
+    std::cout << first << " " << symbol << " " << second
+              << " equals: " << getAnswer(first, second, symbol) << std::endl;
+  } else {
+    std::cout << "Hmm.. something went wrong!" << std::endl;
+    return 1;
+  }
+
+  return 0;
+}
