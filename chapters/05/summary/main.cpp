1b74186ea871580cb1be719f19b8fabbfb7649e9
Harvey Reynier <56921263+hreynier@users.noreply.github.com>
2025-08-17 21:20:07 +0100

    better version

--- chapters/05/summary/main.cpp
+++ chapters/05/summary/main.cpp
@@ -6,35 +6,49 @@
 #include <string>
 #include <string_view>
 
+void printNameAndAge(std::string_view n1, std::string_view n2, int a1, int a2) {
+  std::cout << n1 << " (age " << a1 << ") is older than " << n2 << " (age "
+            << a2 << ") \n";
+}
+
 void printOlderPerson(std::string_view name1, std::string_view name2, int age1,
                       int age2) {
   if (age1 > age2) {
-    std::cout << name1 << " (age " << age1 << ") is older than " << name2
-              << " (age" << age2 << ") \n";
+    printNameAndAge(name1, name2, age1, age2);
   } else if (age2 > age1) {
 
-    std::cout << name2 << " (age " << age2 << ") is older than " << name1
-              << " (age" << age1 << ") \n";
+    printNameAndAge(name2, name1, age2, age1);
   } else {
-    std::cout << name1 << "(age " << age1 << ") is the same age as " << name2
+    std::cout << name1 << " (age " << age1 << ") is the same age as " << name2
               << " (age" << age2 << ") \n";
   }
 }
 
+std::string getName(int n) {
+  std::cout << "Enter the name of person #" << n << ": ";
+  std::string name{};
+  std::getline(std::cin >> std::ws, name);
+  return name;
+}
+
+int getAge(std::string_view name) {
+  std::cout << "Enter the age of " << name << ": ";
+  int age{};
+  std::cin >> age;
+  return age;
+}
+
 int main() {
-  std::string name1{}, name2{};
-  int age1{}, age2{};
+  constexpr int FIRST = 1;
+  constexpr int SECOND = 2;
 
-  std::cout << "Enter the name of person #1: ";
-  std::getline(std::cin >> std::ws, name1);
-  std::cout << "Enter the name of person #2: ";
-  std::getline(std::cin >> std::ws, name2);
-  std::cout << "Enter the age of " << std::string_view(name1) << " :";
-  std::cin >> age1;
-  std::cout << "Enter the age of " << std::string_view(name2) << " :";
-  std::cin >> age2;
+  std::string n1{getName(FIRST)};
+  int a1{getAge(n1)};
 
-  printOlderPerson(name1, name2, age1, age2);
+  std::string n2{getName(SECOND)};
+  int a2{getAge(n2)};
+
+  printOlderPerson(n1, n2, a1, a2);
 
   return 0;
 }
